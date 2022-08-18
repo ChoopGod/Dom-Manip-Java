d86f90f2fd1b186ce3769feb764945d84c9144da
Choop God <93056218+ChoopGod@users.noreply.github.com>
2022-08-18 12:21:17 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
+//declaring my elements and variables
 const myApp=document.getElementById("app")
 const newFigure = document.createElement("figure");
 const galleryCard = document.createElement("img");
 const description = document.createElement("p");
-
+//this gives the img tag we created its source
 galleryCard.setAttribute(
     'src',
     '/PFP.webp'
 );
-
+//appending childs and giving the h2 and p tags their text
 myApp.appendChild(newFigure)
 newFigure.innerHTML="<H2>My Profile Picture</H2>"
 newFigure.appendChild(galleryCard)
