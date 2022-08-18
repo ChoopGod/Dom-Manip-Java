99f3dda5aebbe91a0c169a5c89a9764c9a463c65
Choop God <93056218+ChoopGod@users.noreply.github.com>
2022-08-18 09:54:09 +0200

    renamed to galleryCard
    renamed image to gallery card for the assignment and created the gallerycard class in css

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 const myApp=document.getElementById("app")
 const newFigure = document.createElement("figure");
-const image = document.createElement("img");
+const galleryCard = document.createElement("img");
 const description = document.createElement("p");
 
-image.setAttribute(
+galleryCard.setAttribute(
     'src',
     '/PFP.webp'
 );
 
 myApp.appendChild(newFigure)
 newFigure.innerHTML="<H2>My Profile Picture</H2>"
-newFigure.appendChild(image)
+newFigure.appendChild(galleryCard)
 description.innerText="This is my profile picture on discord"
 newFigure.appendChild(description)
